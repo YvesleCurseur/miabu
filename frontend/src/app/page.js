55b1e803354afaec9076ea3a0b084f866dbfcfb7
YvesleCurseur <pognonyvesfulbert@gmail.com>
2023-05-29 17:08:53 +0000

    Push to prod somehow

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -5,29 +5,23 @@ import Sidebar from '@/components/Sidebar';
 import { useEffect, useState } from 'react'
 import Feed from '@/components/Feed';
 import { getHelloWorld } from './api/hello-world/route';
+import Head from 'next/head';
 
 export default function Home() {
-
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    getHelloWorld().then((response) => {
-      setMessage(response.message);
-    })
-  }, []);
   
   return (
+    <>
     <div className='flex'>
       {/* <Sidebar /> */}
       <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
           Partager vos épreuves.
           <br className="max-md:hidden"/>
-          {/* <span className="orange_gradient">AI</span> */}
         </h1>
         <p className="desc text-center">Vous avez la possibilié de partager et de télécharger vos épreuves</p>
-        <Feed />
+        {/* <Feed /> */}
       </section>
-    </div> 
+    </div>
+    </> 
   )
 }
